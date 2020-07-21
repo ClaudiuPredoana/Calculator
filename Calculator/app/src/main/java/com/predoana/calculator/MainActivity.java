26c1309f637140d0dec34cdbb88ccd16cfc0b361
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-07-21 16:58:05 +0300

    Set click listener on the button

--- Calculator/app/src/main/java/com/predoana/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/predoana/calculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.predoana.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,11 +15,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText editText = findViewById(R.id.et_firstNumber);
-        EditText editText1 = findViewById(R.id.et_secondNumber);
+        final EditText editTextFirstValue = findViewById(R.id.et_firstNumber);
+        final EditText editTextSecondValue = findViewById(R.id.et_secondNumber);
         Button button = findViewById(R.id.btn_add);
-        TextView textView = findViewById(R.id.result);
-        
+        final TextView textViewResult = findViewById(R.id.result);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String firstValue = editTextFirstValue.getText().toString();
+                String secondValue = editTextSecondValue.getText().toString();
+
+               int result = Integer.valueOf(firstValue) + Integer.valueOf(secondValue);
+                textViewResult.setText(String.valueOf(result));
+            }
+        });
+
 
     }
 }
