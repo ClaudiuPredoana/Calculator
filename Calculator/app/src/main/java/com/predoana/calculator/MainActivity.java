314901612cca2d3150b3d6cd39e0489f97efe844
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-07-21 16:42:41 +0300

    Referenced all the views

--- Calculator/app/src/main/java/com/predoana/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/predoana/calculator/MainActivity.java
@@ -3,6 +3,9 @@ package com.predoana.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText = findViewById(R.id.et_firstNumber);
+        EditText editText1 = findViewById(R.id.et_secondNumber);
+        Button button = findViewById(R.id.btn_add);
+        TextView textView = findViewById(R.id.result);
+        
+
     }
 }
